2a26331b3daf3f0aef244844d995c499eb7dcb85
jasperan <23caj23@gmail.com>
2024-02-01 01:20:40 +0100

    feat: added initial script

---
+++ automation/initial_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mv setup-novnc-livelabs/config-gnome-desktop.sh setup-novnc-livelabs/.config-gnome-desktop.sh 
+mv setup-novnc-livelabs/set-env-db.sh setup-novnc-livelabs/.set-env-db.sh 
+mv setup-novnc-livelabs/set-env.sh setup-novnc-livelabs/.set-env.sh 
