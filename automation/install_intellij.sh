9e7e045e665bb1e5a01ef96deab19e47fe0738a4
jasperan <23caj23@gmail.com>
2024-02-02 03:03:59 +0100

    feat: added preemptive sleeps

--- automation/install_intellij.sh
+++ automation/install_intellij.sh
@@ -1,6 +1,7 @@
 sudo yum install epel-release
 sudo yum install snapd
-sleep 5s
+sleep 10s
 sudo systemctl enable --now snapd.socket
+sleep 10s
 sudo ln -s /var/lib/snapd/snap /snap
 sudo snap install intellij-idea-community --classic
